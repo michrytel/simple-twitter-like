a5a776bd6f7f70042f1f869504b924f77309720b
michrytel <76482065+michrytel@users.noreply.github.com>
2021-04-08 20:20:29 +0200

    npm modal internet connection - full

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -2,17 +2,12 @@ import React, {useState} from "react";
 
 
 const Error = () => {
-    const [connection, setConnection] = useState<boolean>(true)
-    const isOnline = require('is-online');
-    (async () => {
-        setConnection(await isOnline());
-    })();
-    if (connection) {
+    if (navigator.onLine) {
         return null
     } else {
         return (
             <div className="error__wrapper">
-                <h1>Sorry, check your internet conneccion and try again</h1>
+                <h1>Sorry, check your internet connection and refresh page</h1>
             </div>
         )
     }
