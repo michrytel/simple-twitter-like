2e4e71c10bdd3aa053fe551da9d477c3b3fc7cd2
michrytel <76482065+michrytel@users.noreply.github.com>
2021-04-09 11:53:40 +0200

    Update App.test.tsx

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -4,6 +4,7 @@ describe('App component test', () => {
     it ("Renders without crashing", () => {
         const div = document.createElement("div");
         ReactDOM.render(<App/>, div)
+        expect(div.childElementCount).toBe(1)
         ReactDOM.unmountComponentAtNode(div);
     })
 });
